430486cbc847d53d01fd247dc1f04da35e5f5b0a
Henry Unite <unitehenry@gmail.com>
2020-07-13 05:48:11 -0400

    refactor service

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
+// Dependencies
 const puppeteer = require('puppeteer');
-const cookies = require('./cookies');
 
-(async () => {
+/** 
+* Break existing cookies into individual pieces for puppeteer to use 
+* @returns {Array} Individual cookies separated by name and value
+*/
+function cookieMonster(cookies) {
+ return new Promise((resolve, reject) => {
+    try {
+      const crumbs = cookies.split(';').map(c => {
+        const crumb = c.split('=');
+        return { name: crumb[0], value: crumb[1] };
+      });
+    
+      resolve(crumbs);
+    } catch (err) {
+      reject(err);
+    }
+ }) 
+}
 
-  const crumbs = cookies.split(';').map(c => {
-    const nible = c.split('=');
-    return { name: nible[0], value: nible[1] };
-  });
+// Program
+async function main() {
 
+  // Initiate browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+ 
+  // Establish cookies
+  const cookies = await cookieMonster(require('./cookies'); 
+  cookies.forEach(async (c) => await page.setCookie(c));
 
-  crumbs.forEach(async (c) => await page.setCookie(c));
-
+  // Capture DnD Beyond Image
   await page.goto('https://dndbeyond.com/');
   await page.screenshot({path: 'snap.png'});
 
+  // Finalize browser
   await browser.close();
 
-})()
+}
+
+// Start the Program
+main();
